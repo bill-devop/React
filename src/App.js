43234209ec579d4021608fdfe4c677589a3d7783
bill-devop <muirhill5@gmail.com>
2022-03-03 20:08:29 -0600

    Added expenses data to App.js and passed it as a props object to ExpenseItem.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  // passing data via 'props'
+
+  const expenses = [
+    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: "Towel", amount: 12.29, date: new Date(2021, 3, 23) },
+    { title: "Toilet Paper", amount: 8.65, date: new Date(2021, 3, 3) },
+    { title: "Wooden Table", amount: 2733.75, date: new Date(2020, 6, 26) },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,25 @@
-import './ExpenseItem.css';
+// In this component, we are pointing up. Our use of 'props' as a parameter is allowing us to take the data created from where we are pointing, which is inside App.js. 
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
+import "./ExpenseItem.css";
 
-    // Using dynamic placeholders to point to data defined outside the return statement.
+// passing 1 paramater, which holds all the attribute properties for the obejct. Contains key/value pairs
+function ExpenseItem(props) {
 
-    return (
-        <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
-            <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
-            </div>
-        </div>
-    )
+
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
+  // using the parameter, 'props' as used here, to call the attribute set in App.js. We use the key from where this function gets called, in this case, inside of App.js. There it contains 3: title, amount, date.
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
