6eb39a4a7fd48ee397543e72751e85caad1ffcde
bill-devop <muirhill5@gmail.com>
2022-03-03 20:55:26 -0600

    Added Card.js to simplify code and using props.children reserved name to create enclosing div

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import "./Card.css";
+
+function Card(props) {
+  // using the props key className values inside the using component and any of its css.
+  const classes = "card " + props.className;
+
+  // using the reserve name 'children' in order to make this an enclosing div in other components
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   // now we create attributes for the ExpenseDate like we did in App.js. This will pass props on to that component. This is how you pass on. You cannot skip. Forwarding.
   return (
-    <div className="expense-item">
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
