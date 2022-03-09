3fad35ba43785a72cb2a83eed05b5f538ffa36c3
bill-devop <muirhill5@gmail.com>
2022-03-08 20:18:23 -0600

    Adding keys for list items

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import React, { useState } from "react";
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+  { title: "Towel", amount: 12.29, date: new Date(2021, 3, 23) },
+  { title: "Toilet Paper", amount: 8.65, date: new Date(2021, 3, 3) },
+  { title: "Wooden Table", amount: 2733.75, date: new Date(2020, 6, 26) },
+];
+
 const App = () => {
-  const expenses = [
-    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: "Towel", amount: 12.29, date: new Date(2021, 3, 23) },
-    { title: "Toilet Paper", amount: 8.65, date: new Date(2021, 3, 3) },
-    { title: "Wooden Table", amount: 2733.75, date: new Date(2020, 6, 26) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("inapp.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
