76ac59971b82b3bd599f3630d83aec69e09641bd
bill-devop <muirhill5@gmail.com>
2022-03-09 17:29:50 -0600

    Adding button to display add expense form and cancel button to hide it

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -63,7 +63,8 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type="submit">Submit</button>
+        <button type='button' onClick={props.onCancel}>Cancel</button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
